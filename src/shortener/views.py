9bed7c44f95a0b42db70ebd2ee00f7f71f57958b
mikeross6233m <mikeross6233m@gmail.com>
2017-07-07 17:52:09 +0530

    Initial Commit

---
+++ src/shortener/views.py
@@ -0,0 +1,21 @@
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404
+from django.views import View
+
+from .models import KirrURL
+
+# Create your views here.
+# def kirr_redirect_view(request, shortcode=None, *args, **kwargs):
+# 	obj = get_object_or_404(KirrURL, shortcode=shortcode)
+# 	return HttpResponseRedirect(obj.url)
+
+class HomeView(View):
+	def get(self, request, *args, **kwargs):
+		return render(request, "shortener/home.html", {})
+				
+class KirrCBView (View):
+	"""docstring for KirrCBView"""
+	def get(self, request, shortcode=None, *args, **kwargs):
+		obj = get_object_or_404(KirrURL, shortcode=shortcode)
+		return HttpResponseRedirect(obj.url)
+		
